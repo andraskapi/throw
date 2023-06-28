eb5543922a2ff8e3ac8cfc8f5fe8c2fed29260c7
andra <andras_kapitany@hotmail.hu>
2023-06-28 18:12:28 +0200

    kesz v1

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,14 @@
 public class Program {
     public static void main(String[] args) throws Exception {
         Szamit szamit = new Szamit();
-        szamit.duplaz(-3.8);
-        System.out.println(szamit.duplaz(-3.8));
+
+        System.out.println(szamit.duplaz(5));
+
+        try {
+           szamit.duplaz(-3.8);
+        }catch (NegativSzamException exception){
+            System.out.println(exception.getMessage());
+        }
+
     }
 }
